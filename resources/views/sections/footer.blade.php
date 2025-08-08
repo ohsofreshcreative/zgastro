05e45d463638f981bf73cef71ae3062043e2d34c
ohsofreshcreative <132752926+ohsofreshcreative@users.noreply.github.com>
2025-08-08 14:06:41 +0200

    Automatyczny deploy

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -14,7 +14,7 @@
 	<div class="c-main flex flex-col md:flex-row justify-between gap-6 py-10 footer-bottom">
 		<p class="">Copyright ©2025 <?php echo get_bloginfo('name'); ?>. All Rights Reserved</p>
 		<p class="flex gap-2">Designed &amp; Developed by
-			<a target="_blank" href="https://www.ohsofresh.pl" title="OhSoFresh"><img class="oh" src="/wp-content/themes/raab/resources/images/ohsofresh.svg"></a>
+			<a target="_blank" href="https://www.ohsofresh.pl" title="OhSoFresh"><img class="oh" src="/wp-content/themes/kreacja/resources/images/ohsofresh.svg"></a>
 		</p>
 	</div>
 	</div>
