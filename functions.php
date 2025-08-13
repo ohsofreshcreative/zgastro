600606a6a834ac619c5f9dbe0d352555c62a5898
ohsofreshcreative <132752926+ohsofreshcreative@users.noreply.github.com>
2025-08-13 09:46:46 +0200

    Automatyczny deploy

--- functions.php
+++ functions.php
@@ -104,3 +104,8 @@ collect(['setup', 'filters'])
 add_filter('sage/acf-composer/fields', fn () => [
     App\Blocks\ExampleBlock::class,
 ]);
+
+
+add_action('after_setup_theme', function () {
+    add_theme_support('woocommerce');
+});
