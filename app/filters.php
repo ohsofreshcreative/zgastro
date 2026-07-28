6d430afe73209e91b4f52f6bacd99ce3fd0b40d6
Arek <kontakt@ohsofresh.pl>
2026-07-28 16:29:19 +0200

    Automatyczny deploy

--- app/filters.php
+++ app/filters.php
@@ -31,3 +31,37 @@ add_filter('gettext', function ($translated_text, $text, $domain) {
 add_filter('loop_shop_per_page', function ($cols) {
     return 18;
 }, 20);
+
+
+add_filter('acf/update_value/type=wysiwyg', function ($value, $postId, $field) {
+    if (! is_string($value) || $value === '') {
+        return $value;
+    }
+
+    $allowedHtml = [
+        'p' => [],
+        'br' => [],
+        'strong' => [],
+        'b' => [],
+        'em' => [],
+        'i' => [],
+        'u' => [],
+        's' => [],
+        'blockquote' => [],
+        'ul' => [],
+        'ol' => [],
+        'li' => [],
+        'h2' => [],
+        'h3' => [],
+        'h4' => [],
+        'h5' => [],
+        'a' => [
+            'href'   => true,
+            'title'  => true,
+            'target' => true,
+            'rel'    => true,
+        ],
+    ];
+
+    return wp_kses($value, $allowedHtml);
+}, 10, 3);
